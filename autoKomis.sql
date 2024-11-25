bc191dd9fd6bdb54e17ae90c5dfe7c7021514f91
RC131413 <rcebula123@gmail.com>
2024-11-25 23:06:48 +0100

    dobra baza

--- autoKomis.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Pracownicy (pesel, imie, nazwisko, StanowiskoID, PlacowkaID, zarobki)
-VALUES
-('11223344556', 'Grzegorz', 'Bartosz', 2, 1, 3700.00), -- Drugi Sprzedawca w Placówce 1
-('22334455667', 'Joanna', 'Malisz', 3, 2, 5200.00),   -- Drugi Mechanik w Placówce 2
-('33445566778', 'Zofia', 'Jankowska', 4, 3, 4300.00), -- Drugi Lakiernik w Placówce 3
-('44556677889', 'Michał', 'Nowicki', 7, 4, 5400.00),  -- Drugi Elektronik w Placówce 4
-('55667788990', 'Natalia', 'Kaczmarek', 1, 1, 6500.00); -- Drugi Kierownik w Placówce 1
